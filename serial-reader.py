4c210e35148e09266c6bfbf3c88dfefde7874cf9
Adam Selker <adamselker@gmail.com>
2019-04-04 18:02:12 -0400

    defourierfy

--- serial-reader.py
+++ serial-reader.py
@@ -4,14 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 with serial.Serial('/dev/ttyACM0') as s:
+  data = []
   while True:
-    data = []
-    for _ in range(1000):
+    for _ in range(200):
       data += [int(s.readline())]
+    data = data[-2000:]
 
+    """
     fourier = np.fft.rfft(data)
     plt.plot(np.fft.rfftfreq(len(data), 0.01), abs(fourier))
-    plt.pause(0.05)
+    """
+    plt.plot(np.arange(0, len(data)/500, 1/500), data)
+    plt.ylim(-200, 1500)
+    plt.pause(0.0001)
     plt.cla()
 
 plt.show()
