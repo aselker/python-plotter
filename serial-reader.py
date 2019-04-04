b2955f5005f38d909caacfd86be421b9e0436b84
Adam Selker <adamselker@gmail.com>
2019-04-04 11:16:33 -0400

    plot things

--- serial-reader.py
+++ serial-reader.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-
 import serial
+import numpy as np
+import matplotlib.pyplot as plt
 
 with serial.Serial('/dev/ttyACM0') as s:
   while True:
-    r = int(s.readline())
-    print(r)
+    data = []
+    for _ in range(100):
+      data += [int(s.readline())]
 
+    fourier = np.fft.rfft(data)
+    plt.plot(abs(fourier))
+    plt.pause(0.05)
+    plt.cla()
 
+plt.show()
