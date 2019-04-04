a711fb41408a15928f45ad1dc2b590237bc556c3
Adam Selker <adamselker@gmail.com>
2019-04-04 11:41:50 -0400

    better data

--- serial-reader.py
+++ serial-reader.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 with serial.Serial('/dev/ttyACM0') as s:
   while True:
     data = []
-    for _ in range(100):
+    for _ in range(1000):
       data += [int(s.readline())]
 
     fourier = np.fft.rfft(data)
-    plt.plot(abs(fourier))
+    plt.plot(np.fft.rfftfreq(len(data), 0.01), abs(fourier))
     plt.pause(0.05)
     plt.cla()
 
