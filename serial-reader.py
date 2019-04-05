c63109d6751bd81ddb3e90937735ec90be37a328
Adam Selker <adamselker@gmail.com>
2019-04-05 11:03:50 -0400

    save data

--- serial-reader.py
+++ serial-reader.py
@@ -2,21 +2,38 @@
 import serial
 import numpy as np
 import matplotlib.pyplot as plt
+import csv
+
+def moving_average(a, n=3) :
+    ret = np.cumsum(a, dtype=float)
+    ret[n:] = ret[n:] - ret[:-n]
+    return ret[n - 1:] / n
 
 with serial.Serial('/dev/ttyACM0') as s:
   data = []
   while True:
-    for _ in range(200):
-      data += [int(s.readline())]
-    data = data[-2000:]
+    try:
+      for _ in range(200):
+        data += [int(s.readline())]
+      # data = data[-5000:]
 
-    """
-    fourier = np.fft.rfft(data)
-    plt.plot(np.fft.rfftfreq(len(data), 0.01), abs(fourier))
-    """
-    plt.plot(np.arange(0, len(data)/500, 1/500), data)
-    plt.ylim(-200, 1500)
-    plt.pause(0.0001)
-    plt.cla()
+      """
+      fourier = np.fft.rfft(data)
+      plt.plot(np.fft.rfftfreq(len(data), 0.002), abs(fourier))
+      plt.xlim(0, 3)
+      plt.ylim(0, 800000)
+      """
+      smoothed = moving_average(data, 40)
+      # plt.plot(np.arange(0, len(smoothed)/500, 1/500), smoothed)
+      plt.plot(smoothed)
+      plt.ylim(-200, 4096)
+      plt.pause(0.0001)
+      plt.cla()
+
+    except KeyboardInterrupt:
+      with open('data.csv', 'w') as f:
+        for x in data:
+          f.write(str(x) + "\n")
+      break
 
 plt.show()
