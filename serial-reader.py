338ed96b4a47a7137c5141cdfeefc1285b6d9331
Adam Selker <adamselker@gmail.com>
2019-05-05 15:13:33 -0400

    Real low-pass filter

--- serial-reader.py
+++ serial-reader.py
@@ -3,6 +3,7 @@ import serial
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
+from scipy.signal import butter, lfilter, freqz
 
 
 def moving_average(a, n=3):
@@ -11,9 +12,24 @@ def moving_average(a, n=3):
     return ret[n - 1 :] / n
 
 
+# Credit: https://stackoverflow.com/questions/25191620/creating-lowpass-filter-in-scipy-understanding-methods-and-units
+def butter_lowpass(cutoff, fs, order=5):
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype="low", analog=False)
+    return b, a
+
+
+def butter_lowpass_filter(data, cutoff, fs, order=5):
+    b, a = butter_lowpass(cutoff, fs, order=order)
+    y = lfilter(b, a, data)
+    return y
+
+
 chunk_size = 60
-num_chunks = 12
+num_chunks = 20
 chunks_to_drop = 1
+freq = 450  # Hz
 
 with serial.Serial("/dev/ttyACM0") as s:
     for _ in range(chunks_to_drop * chunk_size):
@@ -28,17 +44,19 @@ with serial.Serial("/dev/ttyACM0") as s:
         ax1 = plt.subplot(2, 1, 1)
         ax2 = plt.subplot(2, 1, 2)
 
+        plt.sca(ax2)  # Set current axes
+        smoothed = butter_lowpass_filter(dataToShow, 10, freq)
+        # plt.plot(smoothed[200:])
+        plt.plot(dataToShow)
+        # plt.xlim(120, len(smoothed))
+        # plt.ylim(700, 800)
+
         plt.sca(ax1)  # Set current axes
         fourier = np.fft.rfft(dataToShow)
-        plt.plot(np.fft.rfftfreq(len(dataToShow), 0.001), abs(fourier))
-        plt.xlim(0, 3)
-        plt.ylim(0, 400000)
-
-        plt.sca(ax2)  # Set current axes
-        smoothed = moving_average(dataToShow, 1)
-        # plt.plot(np.arange(0, len(smoothed)/500, 1/500), smoothed)
-        plt.plot(smoothed)
-        # plt.ylim(-200, 4096)
+        # fourier = np.fft.rfft(smoothed)
+        plt.plot(np.fft.rfftfreq(len(dataToShow), 1 / freq), abs(fourier))
+        plt.xlim(0, 5)
+        plt.ylim(0, 10000)
 
         plt.pause(0.01)
         plt.clf()
